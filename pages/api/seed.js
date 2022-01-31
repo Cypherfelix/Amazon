a64b90dd60bdab1fd9a9a70b027d129914601e2d
Cypherfelix <felixmarvinonline@gmail.com>
2022-01-31 14:39:07 +0300

    User Model and database

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -2,13 +2,16 @@ import nc from "next-connect";
 import Product from "../../models/Product";
 import db from "../../utils/db";
 import data from "../../utils/data";
+import User from "../../models/User";
 
 const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
   await Product.deleteMany();
+  await User.deleteMany();
   await Product.insertMany(data.products);
+  await User.insertMany(data.users);
   await db.disconnect();
   res.send({ message: "seeded successfully" });
 });
