a6edc3079156f6a92e986bf706298d3a17379e5c
Cypherfelix <felixmarvinonline@gmail.com>
2022-02-08 22:27:11 +0300

    Place Order Api Post

--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,21 @@ import axios from "axios";
 import { useContext } from "react";
 import { Store } from "../utils/Store";
 import { useSnackbar } from "notistack";
+import { useRouter } from "next/router";
+import { getError } from "../utils/errors";
 
 export default function Home(props) {
+  const router = useRouter();
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const { products } = props;
+  const { products, error } = props;
   const { state, dispatch } = useContext(Store);
   const addToCartHandler = async (product) => {
     const existItem = state.cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
     if (data.countInStock < quantity) {
-      window.alert("Sorry Product is Out of Stock");
+      closeSnackbar();
+      enqueueSnackbar("Sorry Product is Out of Stock");
       return;
     }
     closeSnackbar();
@@ -42,6 +46,24 @@ export default function Home(props) {
 
     //router.push("/cart");
   };
+
+  if (error != null) {
+    closeSnackbar();
+    enqueueSnackbar(getError(error), { variant: "error" });
+    return (
+      <Layout>
+        <Button
+          size="large"
+          color="primary"
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          Refresh
+        </Button>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
@@ -83,12 +105,23 @@ export default function Home(props) {
   );
 }
 export async function getServerSideProps() {
-  await db.connect();
+  try {
+    await db.connect();
+  } catch (err) {
+    return {
+      props: {
+        products: null,
+        error: err,
+      },
+    };
+  }
+
   const products = await Product.find({}).lean();
   await db.disconnect();
   return {
     props: {
       products: products.map(db.convertDocToObj),
+      error: null,
     },
   };
 }
